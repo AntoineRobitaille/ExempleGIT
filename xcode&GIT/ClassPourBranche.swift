793af0f534e036d42c53762ba494b10312df9f7e
eleves <you@yourdomain.com>
2017-07-03 09:00:39 -0400

    v2

---
+++ xcode&GIT/ClassPourBranche.swift
@@ -0,0 +1,13 @@
+//
+//  ClassPourBranche.swift
+//  xcode&GIT
+//
+//  Created by eleves on 2017-07-03.
+//  Copyright © 2017 eleves. All rights reserved.
+//
+
+import Foundation
+
+class ClassPourBranche {
+    
+}
